39e6b135b85cd8544d13752a52eb0f0d2c543387
aquinoelaine918-tech <aquinoelaine918@gmail.com>
2026-02-25 17:29:37 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,13 +1,12 @@
 from flask import Flask, request
+
 app = Flask(__name__)
 
 VERIFY_TOKEN = "tec9_verify_2026"
 
-
 @app.get("/")
 def home():
     return "Tec bot rodando no Render ✅", 200
-
 
 @app.get("/webhook")
 def verify():
@@ -19,3 +18,9 @@ def verify():
         return challenge, 200
 
     return "Token de verificação inválido", 403
+
+@app.post("/webhook")
+def receive():
+    data = request.get_json(silent=True) or {}
+    print("EVENTO RECEBIDO:", data)
+    return "ok", 200
