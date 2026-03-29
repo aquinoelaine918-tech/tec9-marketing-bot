10ae3674e612e06f8b11216660f7058ec3d05754
aquinoelaine918-tech <aquinoelaine918@gmail.com>
2026-03-29 18:55:49 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
-from flask import Flask, request
+from flask import Flask, request, jsonify
 import requests
+import os
 
 app = Flask(__name__)
 
-VERIFY_TOKEN = "tec9token123"
-ACCESS_TOKEN = "esse é o token faz o testo completo incluindo o token EAAM78ivqOXwBRMOHYS3jNxTi8G71eZBafoPsdP9ZAvCqMGJi043Txh0ZAotxSOWjwZAWjO5HtkRqNpI5kNhPdvKZAvWI9OSZC3CBLZAjG6DkgE3n9NeaS4ubC0Dmy4U8PNEiagfNWuVdDFfbEG1nGCWQZALoej1mzmqEhvgQ5mq2D1kzMsZCAaKG2U8P6WKaXZBmBezPsfUe8JhK46MgzXaRveD5zdMkE2JMGqanpm7Qyv4gpEU9BZA2w4u1QXodoALB9ZB5bpZBoSmPg0ZBAQK44F822XTmj7"
-PHONE_NUMBER_ID = "1073214362539680"
+VERIFY_TOKEN = os.getenv("EAAM78ivqOXwBRBExwq2hoHwLiVf0RctWWJJmxnGxnJ2RiBBtSvklyKsFf6yTSY87haYKKaWcX1dpjlmhI67FXQcOq0nAcJgIqJomEYzqfIIrMGHPjWhRxlsUHQTdMzC4aQ3WZARFU9JhAZCgRH25t18ZCrgVFoDUzd5xHg7BZCh82tvBKxZB2ZA3ZBJwBy9ljAYeYYZAh7fCaXlw7xknjkM2vZC55A55QSgvyAOtv6J6waLzmXnVmFHc4zZBqYDptFax4x3UfBE1SCvZBV4TRLHLTyxQ179AAZDZD")
+ACCESS_TOKEN = os.getenv("META_ACCESS_TOKEN")
+PHONE_NUMBER_ID = os.getenv("PHONE_NUMBER_ID")
 
 
 @app.route("/", methods=["GET"])
@@ -18,8 +19,11 @@ def verify():
     token = request.args.get("hub.verify_token")
     challenge = request.args.get("hub.challenge")
 
+    print("TOKEN RECEBIDO:", token)
+    print("VERIFY_TOKEN APP:", VERIFY_TOKEN)
+
     if token == VERIFY_TOKEN:
-        return challenge
+        return challenge, 200
     return "Erro de verificação", 403
 
 
@@ -36,7 +40,10 @@ def webhook():
         if "messages" in value:
             message = value["messages"][0]
             from_number = message["from"]
-            texto = message["text"]["body"]
+
+            texto = "mensagem recebida"
+            if "text" in message:
+                texto = message["text"]["body"]
 
             resposta = f"Olá! 👋 Recebi sua mensagem: {texto}\n\nSou a TEC9 Informática e vou te ajudar agora."
 
@@ -55,13 +62,15 @@ def webhook():
             }
 
             r = requests.post(url, headers=headers, json=payload)
-            print(r.status_code, r.text)
+            print("META STATUS:", r.status_code)
+            print("META RESPOSTA:", r.text)
 
     except Exception as e:
-        print("Erro:", e)
+        print("Erro no POST /webhook:", str(e))
 
     return "ok", 200
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8080)
+    port = int(os.getenv("PORT", 8080))
+    app.run(host="0.0.0.0", port=port)
